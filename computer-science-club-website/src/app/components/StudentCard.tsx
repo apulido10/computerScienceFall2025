eae0f217f917e1442539fac3f4d42496f4d048ee
kj6gcs <kj6gcs@gmail.com>
2025-10-15 19:29:53 -0700

    10-15-25: Added my card.  Changed name/role/major/projects/skills prefix per line on the cards to be amber-300 to differentiate them and align with school colors.  Keeping Alex's yellow-200 at the top for page title (src/page.tsx).  I think the yellow-200 actually works better toward our school colors, so I'm leaving them both for comparison.  I also added a border around the images to help them stand out for now.  Just kicking around ideas.  -Robby-

--- computer-science-club-website/src/app/components/StudentCard.tsx
+++ computer-science-club-website/src/app/components/StudentCard.tsx
@@ -1,39 +1,58 @@
 import { Children } from "react";
 import Image from "next/image";
 
-type StudentInfo= {
-    name?:string;
-    children?: React.ReactNode
-    className?: string;
-    major?: string;
-    projectLinks?: React.ReactNode; 
-    imageSrc: string;
-    role?: string;
-    skills?: string; 
-    about?: string;
-}
+type StudentInfo = {
+  name?: string;
+  children?: React.ReactNode;
+  className?: string;
+  major?: string;
+  projectLinks?: React.ReactNode;
+  imageSrc: string;
+  role?: string;
+  skills?: string;
+  about?: string;
+};
 
-export default function StudentInfo({name,children,className,major,projectLinks,imageSrc,role,skills,about}:StudentInfo){
-    return(
-        <div className={`flex flex-col bg-neutral-900 p-2 items-center justify-center rounded-md font-bold ${className}`}>
-            <div className="text-xl">{name}</div>
-            <Image 
-            src={imageSrc} 
-            alt={name ?? "Student"}
-            width={100}
-            height={200}
-            className="rounded-3xl m-2"
-            />
-            <div>Role: {role}</div>
-            <div>Major: {major}</div>
-            <div className="flex items-center gap-2 overflow-x-auto">
-                <span >Projects:</span>
-                <div className="inline-flex gap-3 whitespace-nowrap">
-                {projectLinks}
-                </div>
-                </div>
-            <div>Skills: {skills}</div>
-            <div>{children}</div>
+export default function StudentInfo({
+  name,
+  children,
+  className,
+  major,
+  projectLinks,
+  imageSrc,
+  role,
+  skills,
+  about,
+}: StudentInfo) {
+  return (
+    <div
+      className={`flex flex-col bg-neutral-900 p-2 items-center justify-center rounded-md font-bold ${className}`}
+    >
+      <div className="text-xl text-amber-300">{name}</div>
+      <Image
+        src={imageSrc}
+        alt={name ?? "Student"}
+        width={100}
+        height={200}
+        className="rounded-3xl m-2 border-2 border-amber-300"
+      />
+      <div className="self-start">
+        <span className="text-amber-300">Role:</span> {role}
+      </div>
+      <div className="self-start">
+        <span className="text-amber-300">Major:</span> {major}
+      </div>
+      <div className="flex items-center gap-2 overflow-x-auto self-start">
+        <span className="text-amber-300">Projects:</span>
+        <div className="inline-flex gap-3 whitespace-nowrap">
+          {projectLinks}
         </div>
-    )
+      </div>
+      <div>
+        <span className="text-amber-300 self-start">Skills: </span>
+        {skills}
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 }
--- computer-science-club-website/src/app/page.tsx
+++ computer-science-club-website/src/app/page.tsx
@@ -1,43 +1,70 @@
 import Image from "next/image";
 import StudentInfo from "./components/StudentCard";
-import alexPicture from "../../public/pictures/alex.jpg"
-
+import alexPicture from "../../public/pictures/alex.jpg";
+import robbyPicture from "../../public/pictures/robby.webp";
 
 export default function Home({}) {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-yellow-200 text-center mt-10 text-2xl font-bold">Computer Science Club</h1>
-      <h1 className="text-yellow-200 text-center mb-10 text-xl">At Butte College</h1>
+      <h1 className="text-yellow-200 text-center mt-10 text-2xl font-bold">
+        Computer Science Club
+      </h1>
+      <h1 className="text-yellow-200 text-center mb-10 text-xl">
+        At Butte College
+      </h1>
 
-    <div>
-      <h1 className="text-center mb-4 text-xl font-bold">Members:</h1>
-     <StudentInfo 
-     className="flex justify-center items-center  mb-10" 
-     name="Alex" 
-     role="Vice President"
-     major="Computer Science"
-     imageSrc={alexPicture.src}
-     projectLinks={
-      <div className="flex gap-2">
-        <a 
-        href="https://www.essentialsites.co/"
-        className="text-blue-400 underline hover:text-blue-200"
-        >Essential Sites
-        </a>
-          <a 
-        href="https://elsapitotaquizasyaguachiles.com/"
-        className="text-blue-400 underline hover:text-blue-200"
-        >Catering Business
-        </a>
+      <div>
+        <h1 className="text-center mb-4 text-xl font-bold">Members:</h1>
+        <StudentInfo
+          className="flex justify-center items-center  mb-10"
+          name="Alex"
+          role="Vice President"
+          major="Computer Science"
+          imageSrc={alexPicture.src}
+          projectLinks={
+            <div className="flex gap-2">
+              <a
+                href="https://www.essentialsites.co/"
+                className="text-blue-400 underline hover:text-blue-200"
+              >
+                Essential Sites
+              </a>
+              <a
+                href="https://elsapitotaquizasyaguachiles.com/"
+                className="text-blue-400 underline hover:text-blue-200"
+              >
+                Catering Business
+              </a>
+            </div>
+          }
+          skills="Next.js, Vercel, PostgreSQL, Typescript"
+        ></StudentInfo>
+
+        <StudentInfo
+          className="flex justify-center items-center  mb-10"
+          name="Robby"
+          role="Secretary / ICC Representative"
+          major="Cybersecurity"
+          imageSrc={robbyPicture.src}
+          projectLinks={
+            <div className="flex gap-2">
+              <a
+                href="https://www.robbywideman.dev"
+                className="text-blue-400 underline hover:text-blue-200"
+              >
+                Portfolio Site
+              </a>
+              <a
+                href="https://starwoven.vercel.app"
+                className="text-blue-400 underline hover:text-blue-200"
+              >
+                Personal Project
+              </a>
+            </div>
+          }
+          skills="Cisco Networking, WebDev, Python, Linux"
+        ></StudentInfo>
       </div>
-     }
-     skills="Next.js, Vercel, PostgreSQL, Typescript"
-   
-     >
- 
-     </StudentInfo>
-     </div>
-
     </div>
   );
 }
